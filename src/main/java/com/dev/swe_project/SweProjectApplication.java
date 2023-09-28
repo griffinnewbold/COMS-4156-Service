e0953bca85b1557a63db34549c2503cfdfe47abf
Griffin Newbold <griffinnewbold@gmail.com>
2023-09-28 10:43:02 -0400

    Initialized Spring Project in IntelliJ

---
+++ src/main/java/com/dev/swe_project/SweProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dev.swe_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/swe_project/SweProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.swe_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
