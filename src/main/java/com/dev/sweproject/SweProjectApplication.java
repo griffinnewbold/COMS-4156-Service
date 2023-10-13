25fbf213fdda478dcae0497eae5c4d70bd85f94d
Griffin Newbold <griffinnewbold@gmail.com>
2023-10-12 23:28:25 -0400

    attempting to make method for generating network ids

--- src/main/java/com/dev/sweproject/SweProjectApplication.java
+++ src/main/java/com/dev/sweproject/SweProjectApplication.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.print.Doc;
 import java.util.HashMap;
 
 
@@ -33,11 +31,12 @@ public class SweProjectApplication {
 		FirebaseService firebaseDatabaseService = context.getBean(FirebaseService.class);
 
 		//Testing DB methods
-		firebaseDatabaseService.createCollection("test-network");
+		firebaseDatabaseService.createCollection("test-network/users");
 
-		firebaseDatabaseService.addEntry("test-network", "testDoc",
+		firebaseDatabaseService.addEntry("test-network/users", "testDoc",
 				new Document("1", null, "012", "the first doc", "txt", 0));
-
+		firebaseDatabaseService.updateEntry("null", "test", null);
+		/*
 		try {
 			CompletableFuture<Object> resultFuture = firebaseDatabaseService.getEntry("test-network", "testDoc");
 			HashMap<String, Object> map = (HashMap<String, Object>) resultFuture.get();
@@ -48,6 +47,8 @@ public class SweProjectApplication {
 		}
 		//firebaseDatabaseService.updateEntry("test-network", "name", "Jeannie");
 		//firebaseDatabaseService.removeEntry("test-network", "name");
+
+		 */
 	}
 
 	/**
