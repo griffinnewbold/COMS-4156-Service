6ea20828275d1b20fed2725ec2286b10dd2def67
Griffin Newbold <griffinnewbold@gmail.com>
2023-10-12 23:38:20 -0400

    fixed missing import of MultiPart file

--- src/main/java/com/dev/sweproject/SweProjectApplication.java
+++ src/main/java/com/dev/sweproject/SweProjectApplication.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.HashMap;
 
 
