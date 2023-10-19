6588a8c52a2772b22d382dd4f5570084c9d9943d
Griffin Newbold <griffinnewbold@gmail.com>
2023-10-19 00:09:17 -0400

    updated javadoc for FirebaseService

--- src/main/java/com/dev/sweproject/FirebaseService.java
+++ src/main/java/com/dev/sweproject/FirebaseService.java
@@ -11,18 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-/*
-CompletableFuture<Object> is a class in Java that represents a future result of
-an asynchronous computation. Part of the java.util.concurrent package and
-Used when you want to retrieve a result once it's available or handle an error if the operation
-fails. https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/CompletableFuture.html
-
-The Firebase API for Java used to mainly run off of Tasks which have since been deprecated
-CompletableFuture was one of the recommended alternatives.
- */
-
 /**
- * Has all service methods related to the DB.
+ * The `FirebaseService` class provides service methods related to the database (DB).
+ * It utilizes `CompletableFuture<Object>` for handling asynchronous operations and representing
+ * future results. This class is part of the `java.util.concurrent` package and is used when you
+ * want to retrieve a result once it's available or handle an error if the operation fails.
+ *
+ * Note: The Firebase API for Java used to primarily rely on Tasks, which have since been deprecated,
+ * and `CompletableFuture` is one of the recommended alternatives.
+ *
+ * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/CompletableFuture.html">CompletableFuture Documentation</a>
  */
 @Service
 public class FirebaseService {
@@ -31,9 +29,9 @@ public class FirebaseService {
   public static final int NETWORK_ID_LENGTH = 3;
 
   /**
-   * Creates an instance of the Firebase Databases.
+   * Creates an instance of the Firebase Service.
    *
-   * @param firebaseApp FirebaseApp object
+   * @param firebaseApp A `FirebaseApp` object representing the Firebase configuration.
    */
   @Autowired
   public FirebaseService(FirebaseApp firebaseApp) {
@@ -41,10 +39,10 @@ public class FirebaseService {
   }
 
   /**
-   * Returns a reference to the database, if multiple clients are using this
-   * should give back the same instance.
+   * Returns a reference to the Firebase Realtime Database. If multiple clients are using this service,
+   * it will provide the same database reference instance.
    *
-   * @return A reference to the Firebase realtime Database.
+   * @return A reference to the Firebase Realtime Database.
    */
   public DatabaseReference getDatabaseReference() {
     FirebaseDatabase database = FirebaseDatabase.getInstance(firebaseApp);
@@ -52,17 +50,17 @@ public class FirebaseService {
   }
 
   /**
-   * Add an entry to the specified collection with the key : value association provided.
+   * Add an entry to the specified collection with the key-value association provided.
    *
-   * @param collection A String denoting which collection this belongs to
-   * @param key A String denoting the key for the association
-   * @param value An object representing the value
+   * @param collectionName A String denoting which collection this entry belongs to.
+   * @param key A String denoting the key for the association.
+   * @param value An object representing the value to be added.
    * @return A CompletableFuture that completes with the value upon successful addition
-   * or an error message
+   *         or completes exceptionally with an error message.
    */
-  public CompletableFuture<Object> addEntry(String collection, String key, Object value) {
+  public CompletableFuture<Object> addEntry(String collectionName, String key, Object value) {
     DatabaseReference databaseReference = getDatabaseReference();
-    DatabaseReference collectionReference = databaseReference.child(collection);
+    DatabaseReference collectionReference = databaseReference.child(collectionName);
 
     CompletableFuture<Object> resultFuture = new CompletableFuture<>();
 
@@ -83,14 +81,14 @@ public class FirebaseService {
   /**
    * Remove an entry from the specified collection with the given key.
    *
-   * @param collection A String denoting which collection this entry belongs to
-   * @param key A String denoting the key for the entry to be removed
+   * @param collectionName A String denoting which collection this entry belongs to.
+   * @param key A String denoting the key for the entry to be removed.
    * @return A CompletableFuture that completes with a success message upon successful removal
-   * or an error message
+   *         or completes exceptionally with an error message.
    */
-  public CompletableFuture<String> removeEntry(String collection, String key) {
+  public CompletableFuture<String> removeEntry(String collectionName, String key) {
     DatabaseReference databaseReference = getDatabaseReference();
-    DatabaseReference collectionReference = databaseReference.child(collection);
+    DatabaseReference collectionReference = databaseReference.child(collectionName);
 
     CompletableFuture<String> resultFuture = new CompletableFuture<>();
 
@@ -110,16 +108,16 @@ public class FirebaseService {
   }
 
   /**
-   * Create a collection in the database.
+   * Create a collection in the database with the specified name.
    *
-   * @param collection A String representing the name of the collection
+   * @param collectionName A String representing the name of the collection to be created.
    * @return A CompletableFuture that completes with the collection name upon successful
-   * creation or an error message
+   *         creation or completes exceptionally with an error message.
    */
-  public CompletableFuture<String> createCollection(String collection) {
+  public CompletableFuture<String> createCollection(String collectionName) {
     DatabaseReference databaseReference = getDatabaseReference();
     Map<String, Object> collectionData = new HashMap<>();
-    collectionData.put(collection, "");
+    collectionData.put(collectionName, "");
 
     CompletableFuture<String> resultFuture = new CompletableFuture<>();
 
@@ -129,8 +127,8 @@ public class FirebaseService {
         System.out.println(errorMessage);
         resultFuture.completeExceptionally(new RuntimeException(errorMessage));
       } else {
-        System.out.println("Collection created successfully: " + collection);
-        resultFuture.complete(collection);
+        System.out.println("Collection created successfully: " + collectionName);
+        resultFuture.complete(collectionName);
       }
     });
 
@@ -138,15 +136,15 @@ public class FirebaseService {
   }
 
   /**
-   * Delete a collection from the database.
+   * Delete a collection from the database with the specified name.
    *
-   * @param collection A String representing the name of the collection
+   * @param collectionName A String representing the name of the collection to be deleted.
    * @return A CompletableFuture that completes with the collection name upon successful
-   * deletion or an error message
+   *         deletion or completes exceptionally with an error message.
    */
-  public CompletableFuture<String> deleteCollection(String collection) {
+  public CompletableFuture<String> deleteCollection(String collectionName) {
     DatabaseReference databaseReference = getDatabaseReference();
-    DatabaseReference collectionReference = databaseReference.child(collection);
+    DatabaseReference collectionReference = databaseReference.child(collectionName);
 
     CompletableFuture<String> resultFuture = new CompletableFuture<>();
 
@@ -156,8 +154,8 @@ public class FirebaseService {
         System.out.println(errorMessage);
         resultFuture.completeExceptionally(new RuntimeException(errorMessage));
       } else {
-        System.out.println("Collection deleted successfully: " + collection);
-        resultFuture.complete(collection);
+        System.out.println("Collection deleted successfully: " + collectionName);
+        resultFuture.complete(collectionName);
       }
     });
 
@@ -165,17 +163,17 @@ public class FirebaseService {
   }
 
   /**
-   * Update an entry in the specified collection with the key : value association provided.
+   * Update an entry in the specified collection with the provided key : value association.
    *
-   * @param collection A String denoting which collection this entry belongs to
-   * @param key A String denoting the key for the association
-   * @param newValue An object representing the new value to be assigned
+   * @param collectionName A String denoting which collection this entry belongs to.
+   * @param key A String denoting the key for the association to be updated.
+   * @param newValue An object representing the new value to be assigned.
    * @return A CompletableFuture that completes with the new value upon successful update
-   * or an error message
+   *         or completes exceptionally with an error message.
    */
-  public CompletableFuture<Object> updateEntry(String collection, String key, Object newValue) {
+  public CompletableFuture<Object> updateEntry(String collectionName, String key, Object newValue) {
     DatabaseReference databaseReference = getDatabaseReference();
-    DatabaseReference collectionReference = databaseReference.child(collection);
+    DatabaseReference collectionReference = databaseReference.child(collectionName);
     DatabaseReference entryReference = collectionReference.child(key);
 
     CompletableFuture<Object> resultFuture = new CompletableFuture<>();
@@ -197,15 +195,16 @@ public class FirebaseService {
   /**
    * Retrieves the value associated with the specified collection and key.
    *
-   * @param collection A String denoting which collection this belongs to
-   * @param key A String denoting the key for the association
-   * @return A CompletableFuture that will be completed with the retrieved value
+   * @param collectionName A String denoting which collection this entry belongs to.
+   * @param key A String denoting the key for the association to be retrieved.
+   * @return A CompletableFuture that will be completed with the retrieved value or
+   *         completes exceptionally with an error message if the value is not found.
    */
-  public CompletableFuture<Object> getEntry(String collection, String key) {
+  public CompletableFuture<Object> getEntry(String collectionName, String key) {
     CompletableFuture<Object> future = new CompletableFuture<>();
 
     DatabaseReference databaseReference = getDatabaseReference();
-    DatabaseReference collectionReference = databaseReference.child(collection);
+    DatabaseReference collectionReference = databaseReference.child(collectionName);
     DatabaseReference entryReference = collectionReference.child(key);
 
     entryReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -230,9 +229,9 @@ public class FirebaseService {
   }
 
   /**
-   * Generates a unique id for a network to use.
+   * Generates a unique network ID to use.
    *
-   * @return A String containing the unique network id
+   * @return A String containing the unique network ID.
    */
   public String generateNetworkId() {
     String timestamp = String.valueOf(System.currentTimeMillis());
@@ -246,9 +245,11 @@ public class FirebaseService {
   }
 
   /**
-   * Searching for a specific document within Firebase
+   * Searches for a specific document within Firebase.
    *
-   * @return A completablefuture object if found or null
+   * @param collectionName A String representing the name of the collection to search in.
+   * @param title A String representing the title of the document to search for.
+   * @return A CompletableFuture object that may contain a DataSnapshot if the document is found, or null.
    */
   public CompletableFuture<DataSnapshot> searchForDocument(String collectionName, String title) {
     CompletableFuture<DataSnapshot> future = new CompletableFuture<>();
@@ -281,9 +282,14 @@ public class FirebaseService {
   }
 
   /**
-   * Uploads a file to Firebase
+   * Uploads a file to Firebase and stores it as a document in the specified collection.
    *
-   * @return A completablefuture object resulting from add entry
+   * @param file The MultipartFile representing the file to be uploaded.
+   * @param collectionName A String representing the name of the collection to upload the file to.
+   * @param fileName A String representing the name of the file.
+   * @param userId A String representing the user ID.
+   * @return A CompletableFuture object that may complete with a result upon successful upload or an error message.
+   * @throws IOException If an IO error occurs during the upload process.
    */
   public CompletableFuture<Object> uploadFile(MultipartFile file, String collectionName,
                                               String fileName, String userId) throws IOException {
